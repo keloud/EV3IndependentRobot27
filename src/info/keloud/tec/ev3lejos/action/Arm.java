ffab3c61979373d5377d38e5da428f2f3c8bfe4f
Keloud <love.barleytea.1007@gmail.com>
2018-12-03 14:35:21 +0900

    アームのコードのリファクタリング

--- src/info/keloud/tec/ev3lejos/action/Arm.java
+++ src/info/keloud/tec/ev3lejos/action/Arm.java
@@ -6,17 +6,10 @@ import lejos.hardware.Sound;
 import static info.keloud.tec.ev3lejos.Main.centerMotor;
 
 public class Arm extends AbstractUtil {
-    private boolean move = false;
-
-    public void run(boolean move) {
-        this.move = move;
-        run();
-    }
-
     @Override
     public void run() {
         // スムーズ移動の設定
-        centerMotor.setAcceleration(6000);
+        centerMotor.setAcceleration(8000);
 
         // 速度設定
         setMaxSpeed(centerMotor.getMaxSpeed());
@@ -26,36 +19,17 @@ public class Arm extends AbstractUtil {
         // trueならば開いている
         if (Main.isArmOpen) {
             setAngle(-360);
-            close();
         } else {
             setAngle(360);
-            open();
         }
-    }
 
-    private void open() {
         try {
-            // モーター回転角を設定し、移動開始
-            centerMotor.rotate((int) getAngle(), move);
+            // 移動開始
+            centerMotor.rotate((int) getAngle(), true);
         } catch (Exception e) {
             Sound.buzz();
         } finally {
-            //Sound.beepSequenceUp();
-            // アームが開いたことにする
-            Main.isArmOpen = true;
-        }
-    }
-
-    private void close() {
-        try {
-            // モーター回転角を設定する
-            centerMotor.rotate((int) getAngle(), move);
-        } catch (Exception e) {
-            Sound.buzz();
-        } finally {
-            //Sound.beepSequenceUp();
-            // アームが閉じたことにする
-            Main.isArmOpen = false;
+            Main.isArmOpen = !Main.isArmOpen;
         }
     }
 }
