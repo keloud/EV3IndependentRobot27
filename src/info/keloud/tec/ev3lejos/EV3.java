876566507fda59f7b69abce800981bd3eda2a6d0
Keloud <love.barleytea.1007@gmail.com>
2018-12-03 15:34:20 +0900

    予選プログラムの修正

--- src/info/keloud/tec/ev3lejos/EV3.java
+++ src/info/keloud/tec/ev3lejos/EV3.java
@@ -18,75 +18,14 @@ class EV3 {
         Button.ENTER.waitForPress();
         sensorUpdater.setStopwatchFlag(true);
 
-        go2();
+        yosen();
 
         // Enterキーを押して次に進む
         Button.ENTER.waitForPress();
         sensorUpdater.stopUpdaterFlag();
     }
 
-    private void go1() {
-        leftMotor.resetTachoCount();
-        rightMotor.resetTachoCount();
-        centerMotor.resetTachoCount();
-        gyroSensor.initGyro();
-
-        Move move = new Move();
-        MoveColor moveColor = new MoveColor();
-        Turn turn = new Turn();
-        Arm arm = new Arm();
-
-        float maxSpeed = 800;
-
-        // アームを空ける
-        arm.run(true);
-        // 真ん中のペットボトルにつっこむ
-        move.run(maxSpeed, 48);
-        // ペットボトルをつかむ
-        arm.run(true);
-        // 置く場所の赤色まで進む
-        moveColor.run(maxSpeed, "RED", true);
-        // ペットボトルを放す
-        arm.run(true);
-        // マップ中央まで進む
-        move.run(maxSpeed, -120);
-        // 右のペットボトルに向く
-        turn.run(maxSpeed, 110);
-        // ペットボトルまでつっこむ
-        move.run(maxSpeed, 35);
-        // ペットボトルをつかむ
-        arm.run(false);
-        // 置く場所にゆっくり向く
-        turn.run(100, -130);
-        // 置く場所の赤色まで進む
-        moveColor.run(maxSpeed, "RED", true);
-        // ペットボトルを放して下がる
-        arm.run(false);
-        move.run(maxSpeed, -15);
-        // マップ中央に向く
-        turn.run(maxSpeed, 10);
-        // マップ中央まで進む
-        move.run(maxSpeed, -105);
-        // 左のペットボトルに向く
-        turn.run(maxSpeed, -110);
-        // ペットボトルまでつっこむ
-        move.run(maxSpeed, 35);
-        // ペットボトルをつかむ
-        arm.run(false);
-        // 置く場所のまでゆっくり向く
-        turn.run(100, 130);
-        // 置く場所の赤色まで進む
-        moveColor.run(maxSpeed, "RED", true);
-        // ペットボトルを放して下がる
-        arm.run(false);
-        move.run(maxSpeed, -15);
-        // マップ中央に向く
-        turn.run(maxSpeed, -15);
-        // 帰宅場所の黄色まで進む
-        moveColor.run(maxSpeed, "YELLOW", false);
-    }
-
-    private void go2() {
+    private void yosen() {
         leftMotor.resetTachoCount();
         rightMotor.resetTachoCount();
         centerMotor.resetTachoCount();
@@ -95,46 +34,46 @@ class EV3 {
         float maxSpeed = 800;
 
         // アームを空ける
-        new Arm().run(true);
+        new Arm().run();
         // 真ん中のペットボトルにつっこむ
         new Move().run(maxSpeed, 48);
         // ペットボトルをつかむ
-        new Arm().run(true);
+        new Arm().run();
         // 置く場所の赤色まで進む
         new MoveColor().run(maxSpeed, "RED", true);
         // ペットボトルを放す
-        new Arm().run(true);
+        new Arm().run();
         // マップ中央まで進む
-        new Move().run(maxSpeed, -170);
+        new Move().run(maxSpeed, -160);
         // 右のペットボトルに向く
         new Turn().run(maxSpeed, 50);
         // ペットボトルまでつっこむ
-        new Move().run(maxSpeed, 45);
+        new Move().run(maxSpeed, 50);
         // ペットボトルをつかむ
-        new Arm().run(false);
+        new Arm().run();
         // 置く場所にゆっくり向く
-        new Turn().run(100, -70);
+        new Turn().run(100, -65);
         // 置く場所の赤色まで進む
         new MoveColor().run(maxSpeed, "RED", true);
         // ペットボトルを放して下がる
-        new Arm().run(false);
+        new Arm().run();
         new Move().run(maxSpeed, -15);
         // マップ中央に向く
         new Turn().run(maxSpeed, 15);
         // マップ中央まで進む
-        new Move().run(maxSpeed, -160);
+        new Move().run(maxSpeed, -145);
         // 左のペットボトルに向く
         new Turn().run(maxSpeed, -50);
         // ペットボトルまでつっこむ
-        new Move().run(maxSpeed, 45);
+        new Move().run(maxSpeed, 50);
         // ペットボトルをつかむ
-        new Arm().run(false);
+        new Arm().run();
         // 置く場所のまでゆっくり向く
-        new Turn().run(100, 70);
+        new Turn().run(100, 65);
         // 置く場所の赤色まで進む
         new MoveColor().run(maxSpeed, "RED", true);
         // ペットボトルを放して下がる
-        new Arm().run(false);
+        new Arm().run();
         new Move().run(maxSpeed, -15);
         // マップ中央に向く
         new Turn().run(maxSpeed, -10);
